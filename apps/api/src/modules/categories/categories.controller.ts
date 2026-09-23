ae90b893e526ad27717995224c9168dad3705d62
Suzuki <visionnative777@gmail.com>
2026-09-22 20:25:31 -0700

    feat: categories functionality

---
+++ apps/api/src/modules/categories/categories.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Role } from '@prisma/client';
+import { Public } from '../../common/decorators/public.decorator';
+import { Roles } from '../../common/decorators/roles.decorator';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto, UpdateCategoryDto } from './dto/category.dto';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(private readonly categories: CategoriesService) {}
+
+  @Public()
+  @Get()
+  list() {
+    return this.categories.list();
+  }
+}
+
+@Controller('admin/categories')
+@Roles(Role.ADMIN)
+export class AdminCategoriesController {
+  constructor(private readonly categories: CategoriesService) {}
+
+  @Post()
+  create(@Body() dto: CreateCategoryDto) {
+    return this.categories.create(dto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateCategoryDto) {
+    return this.categories.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.categories.remove(id);
+  }
+}
